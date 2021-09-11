c6be650f9c24dc9be8c0a334872aa9eaa344f0ce
SergeiVorobev <vorobevse86@gmail.com>
2021-09-11 23:21:56 +0200

    update public folder

---
+++ public/precache-manifest.773680800b3a9e3f5d92155c4a0cc9d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea1ef75d4dc1767cd9c8e7304b18415",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a56b94897147bafa91b3",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a56b94897147bafa91b3",
+    "url": "/static/js/main.ec9ba6e7.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
